b21ae5b4bd5a3bde83a55476993e2650048e8ac5
zhxinc <rdfz_zxc@icloud.com>
2019-06-10 21:19:52 +0800

    Site updated: 2019-06-10 21:19:51

--- script/bootstarp.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.body.addEventListener('touchstart', function () {});
-  var app = new Nlvi(nlviconfig);
-  app.bootstarp();
-});
-$(document).ready(function() {
-  $('.container').show();
-});
